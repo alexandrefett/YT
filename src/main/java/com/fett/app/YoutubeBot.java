41e0f3d91ad541ebe9a94bff23f7ffd427986ea6
Alexandre Fett <alexandrefett@gmail.com>
2021-01-22 11:58:32 +0000

    Update YoutubeBot.java

--- src/main/java/com/fett/app/YoutubeBot.java
+++ src/main/java/com/fett/app/YoutubeBot.java
@@ -16,7 +16,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class YoutubeBot {
-    private String apiKey = "RgSnuSLsXCNwwkxAUBrKFhBSRuX8JMbXdkn6tRGOP08";
+    private String apiKey = "APIkey OrbitProxy";
     private Driver driverType = Driver.CHROME;
     private ExecutorService executor;
     private Database db;
